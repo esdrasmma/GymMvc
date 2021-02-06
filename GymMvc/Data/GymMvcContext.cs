b8d8de7a16745878f96bc140492e6ad2256ac990
Marcelo Esdras <esdrasmma@gmail.com>
2021-02-05 21:07:20 -0300

    feito a primeira entidade e mudado o tema bootstrap

---
+++ GymMvc/Data/GymMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GymMvc.Models;
+
+namespace GymMvc.Data
+{
+    public class GymMvcContext : DbContext
+    {
+        public GymMvcContext (DbContextOptions<GymMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GymMvc.Models.Instructor> Instructor { get; set; }
+    }
+}
--- GymMvc/Startup.cs
+++ GymMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GymMvc.Data;
 
 namespace GymMvc
 {
@@ -33,6 +35,10 @@ namespace GymMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<GymMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("GymMvcContext"), builder =>
+                        builder.MigrationsAssembly("GymMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
