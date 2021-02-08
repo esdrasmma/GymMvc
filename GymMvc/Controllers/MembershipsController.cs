e7e8743bae1b17b971d51ec9cb543a307268ff9f
Marcelo Esdras <esdrasmma@gmail.com>
2021-02-08 16:26:24 -0300

    MembershipsController adicionado

---
+++ GymMvc/Controllers/MembershipsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymMvc.Controllers
+{
+    public class MembershipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
